a3783c762260064774346c27c2a5f6e0f8ff8ee3
kelvinlin <kelvinlin900123@gmail.com>
2021-06-20 10:44:42 +0800

    salesRepo
    - interface for data access to the database

--- src/main/java/com/ebiggerr/cPP/repository/salesRepo.java
+++ src/main/java/com/ebiggerr/cPP/repository/salesRepo.java
@@ -1,4 +1,17 @@
 package com.ebiggerr.cPP.repository;
 
-public interface salesRepo {
+import com.ebiggerr.cPP.entity.upForSales;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface salesRepo extends JpaRepository<upForSales,Long> {
+
+    @Query( value=" SELECT * FROM salesrecord WHERE accountid=?1 AND status='PENDING'",nativeQuery=true)
+    upForSales getByAccountidIsAndStatusIs(long accountid);
+
+    @Query( value = "SELECT * FROM salesrecord WHERE accountid=?1" ,nativeQuery=true)
+    Optional<List<upForSales>> getAllOfAnId(long accountid);
 }
