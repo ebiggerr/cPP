021e301669070e22609072e943a561d291868ee9
kelvinlin <kelvinlin900123@gmail.com>
2021-06-20 10:44:11 +0800

    sales for user to retrieve records and put up new record of sales

--- src/main/java/com/ebiggerr/cPP/controller/sale/saleController.java
+++ src/main/java/com/ebiggerr/cPP/controller/sale/saleController.java
@@ -2,15 +2,15 @@ package com.ebiggerr.cPP.controller.sale;
 
 import com.ebiggerr.cPP.domain.authentication;
 import com.ebiggerr.cPP.domain.request;
+import com.ebiggerr.cPP.domain.submission;
+import com.ebiggerr.cPP.entity.upForSales;
 import com.ebiggerr.cPP.service.sales.salesService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -22,13 +22,28 @@ public class saleController {
         this.salesService=salesService;
     }
 
+    /**
+     *
+     * @param username
+     * @return
+     */
+    @GetMapping(path ="/sales/{username}")
+    public ResponseEntity<List<upForSales>> returnListOfSalesPutUp(@PathVariable String username){
 
+        return new ResponseEntity<>(salesService.getAllPutUpSalesByUsername(username), HttpStatus.OK);
+    }
+
+    /**
+     *
+     * @param userSubmission
+     * @return
+     */
     @PostMapping(path = "/sales")
-    public ResponseEntity<authentication> putUpANewCarIntoTheSystem(@Valid @RequestBody request request){
+    public ResponseEntity<authentication> putUpANewCarIntoTheSystem(@Valid @RequestBody submission userSubmission){
 
         String message = null;
 
-        message = salesService.putUpNewRecord(request);
+        message = salesService.putUpNewRecord(userSubmission);
 
         return new ResponseEntity<authentication>( new authentication(message), HttpStatus.OK);
     }
